9fdb46a6fca99490f1217f06d0a5220c28901201
jiheon2234 <jiheon2234@gmail.com>
2024-08-09 22:09:07 +0900

    상영관 구조 잡기

---
+++ src/main/java/info/jiheon/moviebooking/domain/Screen.java
@@ -0,0 +1,36 @@
+package info.jiheon.moviebooking.domain;
+
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * 상영관
+ */
+public interface Screen {
+
+	/**
+	 * 예약 가능한 좌석 반환
+	 *
+	 * @return 예약가능여부 2차원 배열
+	 */
+	boolean[][] getBookingStatus();
+
+	/**
+	 * 예약 메서드
+	 *
+	 * @param userId   예약자 ID
+	 * @param row      row idx
+	 * @param colStart 예약할 가장 왼쪽 좌석
+	 * @param cnt      예약할 좌석 수
+	 * @return 예약된 좌석 이름들 ex)["A-01"]
+	 */
+	List<String> book(UUID userId, int row, int colStart, int cnt);
+
+	/**
+	 * 예약취소 메서드
+	 *
+	 * @param userId 유저아이디
+	 */
+	void cancelBooking(UUID userId);
+
+}
---
+++ src/main/java/info/jiheon/moviebooking/domain/ScreenFactory.java
@@ -0,0 +1,13 @@
+package info.jiheon.moviebooking.domain;
+
+import static lombok.AccessLevel.*;
+
+import lombok.NoArgsConstructor;
+
+/**
+ * 상영관팩토리
+ */
+@NoArgsConstructor(access = PRIVATE)
+public final class ScreenFactory {
+
+}
